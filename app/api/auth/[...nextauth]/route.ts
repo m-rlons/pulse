3e4e40b6a4df7108a8593be38efd8cfb64885923
marlon wayne <m-rlons@users.noreply.github.com>
2025-06-24 08:29:13 -0400

    feat: Add Resend email provider for authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
 import GitHub from "next-auth/providers/github"
+import Resend from "next-auth/providers/resend"
 
 if (!process.env.AUTH_GITHUB_ID || !process.env.AUTH_GITHUB_SECRET) {
   throw new Error("Missing GitHub OAuth environment variables");
@@ -10,6 +11,9 @@ if (!process.env.AUTH_GITHUB_ID || !process.env.AUTH_GITHUB_SECRET) {
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    Resend({
+      from: "onboarding@resend.dev",
+    }),
     GitHub({
         clientId: process.env.AUTH_GITHUB_ID,
         clientSecret: process.env.AUTH_GITHUB_SECRET,
