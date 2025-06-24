cf248d195952dba215ae93b7fdc380a67b2a88a1
marlon wayne <m-rlons@users.noreply.github.com>
2025-06-24 09:26:25 -0400

    fix: Refactor authOptions to dedicated file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,5 @@
-import NextAuth, { type NextAuthOptions } from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/lib/prisma"
-import GitHub from "next-auth/providers/github"
-import EmailProvider from "next-auth/providers/email"
-import { sendVerificationRequest } from "@/lib/send-verification-request"
-
-if (!process.env.AUTH_GITHUB_ID || !process.env.AUTH_GITHUB_SECRET) {
-  throw new Error("Missing GitHub OAuth environment variables");
-}
-
-if (!process.env.AUTH_RESEND_KEY) {
-  throw new Error("Missing Resend API Key environment variable");
-}
-
-if (!process.env.EMAIL_FROM) {
-    throw new Error("Missing EMAIL_FROM environment variable");
-}
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      from: process.env.EMAIL_FROM,
-      sendVerificationRequest,
-    }),
-    GitHub({
-        clientId: process.env.AUTH_GITHUB_ID,
-        clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
-  ],
-}
+import NextAuth from "next-auth"
+import { authOptions } from "@/lib/auth"
 
 const handler = NextAuth(authOptions)
 
